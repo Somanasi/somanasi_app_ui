df7d294fb3dffc23a420423f3fdcdd6bdfbb0036
paullaster <paullasterokoth98@gmail.com>
2023-03-18 19:56:10 +0300

    add files

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,3 +4,12 @@ import Courses from './Courses'
 import Blog from './Blog'
 import AboutUs from './AboutUs'
 import ContactUs from './ContactUs'
+
+export {
+    Error,
+    Home,
+    Courses,
+    Blog,
+    AboutUs,
+    ContactUs
+};
