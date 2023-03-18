59fa8b7c958896fcae5cdc60140627202b4a2b8f
paullaster <paullasterokoth98@gmail.com>
2023-03-18 21:00:20 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      App
+      <section>
+        <Outlet/>
+      </section>
+    </div>
+
   )
 }
 
