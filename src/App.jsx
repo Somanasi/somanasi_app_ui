6652be6237b8257bf4deb050ab84cf6065f0ef19
paullaster <paullasterokoth98@gmail.com>
2023-03-18 23:01:32 +0300

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Navbar } from './components'
+import { Footer, Navbar } from './components'
 
 const App = () => {
   return (
@@ -9,6 +9,7 @@ const App = () => {
       <section>
         <Outlet/>
       </section>
+      <Footer/>
     </div>
 
   )
