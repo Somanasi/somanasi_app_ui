9d33e7d969a2efb455c9a1d7a2ed0b3a4081dcea
paullaster <paullasterokoth98@gmail.com>
2023-03-19 11:42:10 +0300

    commit changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <span>&COPY;somanasi 2023</span>
+      </div>
     </footer>
   )
 }
