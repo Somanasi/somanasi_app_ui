255eab23bbb244c288125fc799b4e021b4bf238c
paullaster <paullasterokoth98@gmail.com>
2023-03-19 15:44:25 +0300

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { Footer, Navbar } from './components'
 
 const App = () => {
-
+  const [toggleMobileView, setToggleMobileView] = useState(false);
   return (
     <>
       <Navbar />
