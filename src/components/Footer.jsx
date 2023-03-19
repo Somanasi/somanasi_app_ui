cf5cc4f53653caf88fa49f75d309ffc5d16a0d17
paullaster <paullasterokoth98@gmail.com>
2023-03-19 11:48:33 +0300

    commit changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,32 @@ const Footer = () => {
               enim facere in cupiditate, maiores 
           </div>
         </div>
-        <div></div>
-        <div></div>
+        <div>
+        <div>
+          <div>
+            <h3>Other Links</h3>
+          </div>
+          <div>
+              Lorem ipsum dolor sit, amet 
+              consectetur adipisicing elit. 
+              Eligendi eum, ad accusantium 
+              iure molestias iste atque qui quia 
+              enim facere in cupiditate, maiores 
+          </div>
+        </div>
+        <div>
+        <div>
+          <div>
+            <h3>Social</h3>
+          </div>
+          <div>
+              Lorem ipsum dolor sit, amet 
+              consectetur adipisicing elit. 
+              Eligendi eum, ad accusantium 
+              iure molestias iste atque qui quia 
+              enim facere in cupiditate, maiores 
+          </div>
+        </div>
       </div>
       <div>
         <span>somanasi &copy;2023</span>
