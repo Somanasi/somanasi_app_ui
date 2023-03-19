0d9581663862c8e9d72a404fd526ef6a58b3d865
paullaster <paullasterokoth98@gmail.com>
2023-03-19 15:12:45 +0300

    commit changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import Logo from "./Logo"
 const Navbar = () => {
   return (
-    <header>
+    <header className="header">
       <div>
         <Logo/>
       </div>
