a5463443ac90681c415f4e4ca2d26fbc8b0898eb
paullaster <paullasterokoth98@gmail.com>
2023-03-19 11:35:31 +0300

    commit changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header>
       <nav>
         <ul>
-          
+          <NavLink to={'/'}>Home</NavLink>
+          <NavLink to={'/courses'}>Course</NavLink>
         </ul>
       </nav>
     </header>
