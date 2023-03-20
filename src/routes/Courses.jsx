df0c1afba37e9349788ef8607f29d01baeaaba2a
Berclay <berclaym@gmail.com>
2023-03-20 14:55:27 +0300

    Update Courses.jsx
    Tried to make the overfow scrollable. unsuccessful. styles updated
    
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -7,26 +7,31 @@ import {dummyData} from '../dummydata/Dummy.js';
 const Courses = () => {
   return (
     <>
-        <div style={{textAlign:"center", fontSize:"30px"}}><h1>Courses</h1></div>
+    <div style={{overflow:"auto", position:"relative"}}>
+        <div style={{textAlign:"center", fontSize:"25px"}}><h1>Courses</h1></div>
         {dummyData.map((items, index)=>(
-            <div style={{display:"flex", margin:"10px"}}>
-                <div>
-                    <Link to = {"/courses/" + items.id}>
-                        <div>
-                            <h3 style={{padding:"10px"}}>{items.name}</h3>
-                        </div>
-                        <div>
-                            <p>{items.description}</p>
-                        </div>
-                    </Link><hr/>
+            <div style={{width:"80vw", position:"relative"}}>
+                <div style={{display:"flex", margin:"10px"}}>
+                    <div>
+                        <Link to = {"/courses/" + items.id}>
+                            <div>
+                                <h3 style={{padding:"10px"}}>{items.name}</h3>
+                            </div>
+                            <div>
+                                <p>{items.description}</p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div style={{padding:"10px"}}>
+                        <Link to = {"/courses/" + items.id}>
+                            <img width="100px" height="100px" src={items.icon}/>
+                        </Link>
+                    </div>
                 </div>
-                <div style={{padding:"10px"}}>
-                    <Link to = {"/courses/" + items.id}>
-                        <img width="50px" height="50px" src={items.icon}/>
-                    </Link>
-                </div>
+                <hr/>
             </div>
         ))}
+    </div>
     </>
   )
 }
