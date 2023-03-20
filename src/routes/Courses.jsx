820acfa66cefb55ffaf4613812e27232d11a2d31
Berclay <berclaym@gmail.com>
2023-03-20 13:44:48 +0300

    Updated Courses.jsx
    gets courses from dummy data in Dummy.js. No uniform styling done
    
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+//icons are the thumbnail images of the course
+import React from 'react';
+import {Link} from "react-router-dom";
+import '../assets/sass/main.scss'
+import {dummyData} from '../dummydata/Dummy.js';
 
 const Courses = () => {
   return (
-    <div>Courses</div>
+    <>
+        <div style={{textAlign:"center", fontSize:"30px"}}><h1>Courses</h1></div>
+        {dummyData.map((items, index)=>(
+            <div style={{display:"flex", margin:"10px"}}>
+                <div>
+                    <Link to = {"/courses/" + items.id}>
+                        <div>
+                            <h3 style={{padding:"10px"}}>{items.name}</h3>
+                        </div>
+                        <div>
+                            <p>{items.description}</p>
+                        </div>
+                    </Link><hr/>
+                </div>
+                <div style={{padding:"10px"}}>
+                    <Link to = {"/courses/" + items.id}>
+                        <img width="50px" height="50px" src={items.icon}/>
+                    </Link>
+                </div>
+            </div>
+        ))}
+    </>
   )
 }
 
