32ee0d5d088dd3c9f6e0dce2dbd7df476105ba3f
paullaster <paullasterokoth98@gmail.com>
2023-04-08 00:36:22 +0300

    commit somanasi files

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,8 +6,26 @@ const Home = () => {
     <main className='home'>
       <section className='searchBar'>
         <Form method='GET'>
-          <Input></Input>
+          <Input name="search" id="search" >Search here</Input>
         </Form>
+      </section>
+      <section className='tips'>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, aliquam molestias? Sapiente exercitationem 
+            dolorum veniam ipsa debitis accusamus eos est voluptatibus explicabo? Alias facere delectus nobis voluptatibus qui quo cumque.</p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt odit omnis quod 
+            perspiciatis amet quia asperiores commodi quasi eius, quidem, assumenda et nisi adipisci quo dolores officia laudantium fuga repellendus!</p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi laboriosam tempore 
+            earum odio expedita unde consectetur animi sint? Magnam, veritatis. Temporibus, eaque sint enim qui quos esse praesentium porro tempore.</p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident animi a totam delectus, 
+            dicta iusto dolorem eaque nobis. Repellendus voluptate facere porro eveniet aliquam quisquam quaerat ut nemo necessitatibus accusantium!</p>
+        </div>
       </section>
     </main>
   )
