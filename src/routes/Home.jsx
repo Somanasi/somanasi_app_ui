f8a86d3a3fb0fdc791877e8e07c37568e0892da7
paullaster <paullasterokoth98@gmail.com>
2023-04-08 00:30:16 +0300

    commit somanasi files

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Form } from 'react-router-dom'
+import Input from '../components/Input'
 const Home = () => {
   return (
     <main className='home'>
       <section className='searchBar'>
         <Form method='GET'>
-          
+          <Input></Input>
         </Form>
       </section>
     </main>
