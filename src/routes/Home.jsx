742bd9cb86debeb9a73f9f6e93fab7aabb38ab00
paullaster <paullasterokoth98@gmail.com>
2023-04-07 23:59:11 +0300

    commit somanasi files

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className='home'>
+      Lorem ipsum dolor sit amet, consectetur adipiscing
+    </main>
   )
 }
 
