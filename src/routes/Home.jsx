a788a31e3b31a18876ac6af9e327fec628e63da1
Berclay <berclaym@gmail.com>
2023-04-08 11:54:52 +0300

    Update Home.jsx
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>Home</div>
+    <div>ADD WHAT YOU IMAGINE THE MAIN PAGE SHOULD HAVE, ABOUT, SITE DESCRIPTION, ETC</dive>
   )
 }
 
