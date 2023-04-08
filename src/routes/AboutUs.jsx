0b8f44241b0e5079ee2f7523bcce49165e7921f5
Rionge <127012852+Rionge@users.noreply.github.com>
2023-04-08 12:51:15 +0300

    Update AboutUs.jsx
    Signed-off-by: Rionge <127012852+Rionge@users.noreply.github.com>

--- src/routes/AboutUs.jsx
+++ src/routes/AboutUs.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div>AboutUs</div>
-    <div>HERE</div>
+    <div>
+    Welcome to Somesha Dada, an e-learning platform dedicated to providing basic education and skills training to the people of Kenya. Our mission is to empower young girls and adults alike with the knowledge and skills necessary to succeed in a knowledge-based economy, and to prevent unplanned pregnancies and dropout rates among young girls.
+
+At Somesha Dada, we believe that education is the key to unlocking a brighter future for all Kenyans. Our platform offers a range of courses and materials, designed to meet the needs of students at all levels of education. Whether you're a teenager looking to continue your studies or an adult seeking to learn new skills, Somesha Dada has a package that's right for you.
+
+Our platform offers three packages: regular, medium, and premium, each with its own unique features and benefits. From interactive online courses to personalized support and mentorship, Somesha Dada is committed to providing the highest quality education and training to our students.
+
+Join us in our mission to revolutionize e-learning and learning in Africa. Sign up for Somesha Dada today and take the first step towards a brighter future!
+    </div>
   )
 }
 
