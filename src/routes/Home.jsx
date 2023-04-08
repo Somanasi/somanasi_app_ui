71a09d663d4206dd0608948a1549e49ffb7618aa
Rionge <127012852+Rionge@users.noreply.github.com>
2023-04-08 13:07:59 +0300

    Update Home.jsx
    Signed-off-by: Rionge <127012852+Rionge@users.noreply.github.com>

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Home = () => {
   return (
     <div>Home</div>
-    <div>ADD WHAT YOU IMAGINE THE MAIN PAGE SHOULD HAVE, ABOUT, SITE DESCRIPTION, ETC</dive>
+    <div>
+    About Us Section:
+    "Welcome to Somesha Dada, an e-learning platform dedicated to providing basic education and skills training to the people of Kenya. Our mission is to empower young girls and adults alike with the knowledge and skills necessary to succeed in a knowledge-based economy, and to prevent unplanned pregnancies and dropout rates among young girls. At Somesha Dada, we believe that education is the key to unlocking a brighter future for all Kenyans. Our platform offers a range of courses and materials, designed to meet the needs of students at all levels of education. Join us in our mission to revolutionize e-learning and learning in Africa. Sign up for Somesha Dada today and take the first step towards a brighter future!"
+
+    Site Description:
+    "Somesha Dada is an e-learning platform designed to provide basic education and skills training to the people of Kenya. Our platform offers a range of courses and materials, designed to meet the needs of students at all levels of education. Our mission is to empower young girls and adults alike with the knowledge and skills necessary to succeed in a knowledge-based economy, and to prevent unplanned pregnancies and dropout rates among young girls. Our platform offers three packages: regular, medium, and premium, each with its own unique features and benefits. From interactive online courses to personalized support and mentorship, Somesha Dada is committed to providing the highest quality education and training to our students."
+
+    Features Section:
+    "Somesha Dada offers a range of packages, each designed to meet the unique needs of our students. Our Regular package includes access to articles, read-only notes, and counseling to support our learners.
+
+    For a more comprehensive learning experience, our Medium package offers additional features such as audio content in the form of podcasts and songs to aid in memory retention, step-by-step instructions, and video recordings. Medium package subscribers also gain access to two learning communities, allowing them to connect with other learners, form study groups, engage in online chat, and participate in community forums. Learning communities are tailored to specific subjects, providing students with an interactive and engaging learning experience.
+
+    For the ultimate learning experience, our Premium package offers all of the features of the Medium package, as well as video conferencing and access to all learning communities. With Somesha Dada Premium, students can engage in live video discussions with their peers and instructors, enabling them to ask questions and get immediate feedback.
+
+    At Somesha Dada, we are committed to providing the highest quality education and training to our students. Sign up today and discover the package that's right for you."
+    </dive>
   )
 }
 
