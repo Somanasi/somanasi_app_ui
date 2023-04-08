844e93a715715b3ba82e05b058b105b9a8c237d5
Berclay <berclaym@gmail.com>
2023-04-08 11:56:53 +0300

    Update SingleCourse.jsx
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/SingleCourse.jsx
+++ src/routes/SingleCourse.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const SingleCourse = () => {
   return (
     <div>SingleCourse</div>
+    <div>ADD WHAT YOU IMAGINE A SINGLE COURSE SHOULD SHOW, LECTURER, SCHEDULES, MINI DESCRIPTION, IF MINI DESCRIPTION PLEASE WRITE CONTENT YOU WANT TO SEE</div>
   )
 }
 
