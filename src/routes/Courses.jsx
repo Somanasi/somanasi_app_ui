ca72453e9193b3cbe84f3bc206c73ddac1038bdd
Rionge <127012852+Rionge@users.noreply.github.com>
2023-04-08 13:13:04 +0300

    Update Courses.jsx
    Signed-off-by: Rionge <127012852+Rionge@users.noreply.github.com>

--- src/routes/Courses.jsx
+++ src/routes/Courses.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Courses = () => {
   return (
     <div>Courses</div>
-    <div>ADD ALL COURSES YOU WOULD LIKE INCLUDED</div>
+    <div>
+    1. Mathematics
+    2. English
+    3. Kiswahili
+    4. Biology
+    5. Chemistry
+    6. Physics
+    7. Geography
+    8. Business Studies
+    9. History
+    10. Christian Religious Education
+    11. Islamic Religious Educatuion
+    12. French
+    13. Music
+    14. Computer
+    </div>
   )
 }
 
