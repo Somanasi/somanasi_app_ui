29aed7952a433142d968b735ba0d5b5cfe4a2c6d
Rionge <127012852+Rionge@users.noreply.github.com>
2023-04-08 13:28:59 +0300

    Update SingleCourse.jsx
    Signed-off-by: Rionge <127012852+Rionge@users.noreply.github.com>

--- src/routes/SingleCourse.jsx
+++ src/routes/SingleCourse.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const SingleCourse = () => {
   return (
     <div>SingleCourse</div>
-    <div>ADD WHAT YOU IMAGINE A SINGLE COURSE SHOULD SHOW, LECTURER, SCHEDULES, MINI DESCRIPTION, IF MINI DESCRIPTION PLEASE WRITE CONTENT YOU WANT TO SEE</div>
+    <div>
+    Course: Chemistry 
+    Year: form two 
+    Topic: 2.1_Structure of Atom
+    Course Title: [insert course title]
+    Lecturer: [insert name of lecturer or instructor]
+    Schedule: [insert schedule information, such as start date, duration, and time of day for classes or online lessons]
+    Mini Description: [insert brief description of the course, such as:]
+    "In this course, you will learn the basics of [insert subject or topic]. Whether you're a beginner or looking to refresh your knowledge, this course will provide you with the foundational skills you need to succeed. Our expert instructor will guide you through a series of interactive online lessons, designed to help you master the material at your own pace. By the end of this course, you will have gained a solid understanding of [insert subject or topic], and be ready to apply your new skills in a real-world setting.
+    
+    </div>
   )
 }
 
