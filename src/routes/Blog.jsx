4a4690302bdc3ba6a4107a6f9ab0e9719e005082
Berclay <berclaym@gmail.com>
2023-04-08 11:59:56 +0300

    Update Blog.jsx
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Blog = () => {
   return (
     <div>Blog</div>
+    <div>ADD CONTENT YOU'D LIKE TO SEE ON BLOGS LIST PAGE. DESCRIBE BRIEFLY HOW EACH BLOG SHOULD LOOK LIKE IN THE LIST AND ADD CONTENT FOR MINI DESCRIPTION</div>
   )
 }
 
