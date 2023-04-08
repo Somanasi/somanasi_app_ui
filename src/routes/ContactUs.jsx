b4b47573934b59a010cf80ff20bf7e62fc11b32a
Berclay <berclaym@gmail.com>
2023-04-08 11:51:09 +0300

    Update ContactUs.jsx
    Contact content clues
    
    Signed-off-by: Berclay <berclaym@gmail.com>

--- src/routes/ContactUs.jsx
+++ src/routes/ContactUs.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const ContactUs = () => {
   return (
     <div>ContactUs</div>
+    <div>
+    ADD EMAIL
+ADD PHONE NUMBERS, ADD ACCOUNTS, TWITTER, IG, ETC
+</div>
   )
 }
 
