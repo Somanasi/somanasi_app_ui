3157fc04f084c9e9b5e3dcfa0a36b8fb998f047a
Rionge <127012852+Rionge@users.noreply.github.com>
2023-04-08 13:39:41 +0300

    Update Blog.jsx
    Signed-off-by: Rionge <127012852+Rionge@users.noreply.github.com>

--- src/routes/Blog.jsx
+++ src/routes/Blog.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Blog = () => {
   return (
     <div>Blog</div>
-    <div>ADD CONTENT YOU'D LIKE TO SEE ON BLOGS LIST PAGE. DESCRIBE BRIEFLY HOW EACH BLOG SHOULD LOOK LIKE IN THE LIST AND ADD CONTENT FOR MINI DESCRIPTION</div>
+    <div>ADD CONTENT YOU'D LIKE TO SEE ON BLOGS LIST PAGE. DESCRIBE BRIEFLY HOW EACH BLOG SHOULD LOOK LIKE IN THE LIST AND ADD CONTENT FOR MINI DESCRIPTION
+    Blog Title: [insert blog title]
+    Mini Description: [insert brief teaser for the blog post, highlighting the topic or main idea]
+    The blog post should be displayed as a clickable link, with the blog title serving as the anchor text. When the link is clicked, the reader should be taken to the full blog post.
+    
+    Personally I'd prefer the blogs be about teachers and developers, maybe the founding team but not a must
+     </div>
   )
 }
 
