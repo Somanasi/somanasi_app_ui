97e096a7c5570b58a3f3c7f9d0add7ca58c6cc37
paullaster <paullasterokoth98@gmail.com>
2023-04-10 08:12:10 +0300

    make somanasi homepage

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
+import Button from "./Button";
 
 const SearchInput = ({ size = 50 }) => {
   return (
     <div className="form-group">
       <label htmlFor="search"></label>
       <input type="text" name="searchText" id="search" />
-      <span>
+      <Button>
         <FaSearch size={size} />
-      </span>
+      </Button>
     </div>
   );
 };
