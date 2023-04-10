c4b548f42599ac5735eb825e29b63a41418481fd
paullaster <paullasterokoth98@gmail.com>
2023-04-10 19:32:52 +0300

    add auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ import Home from './Home'
 import Courses from './Courses'
 import Blog from './Blog'
 import Login from './Login'
+import Logout from './Logout'
+import Register from './Register'
 import AboutUs from './AboutUs'
 import ContactUs from './ContactUs'
 import SingleBlog from './SingleBlog'
@@ -14,6 +16,9 @@ export {
     Home,
     Courses,
     Blog,
+    Login,
+    Logout,
+    Register,
     AboutUs,
     ContactUs,
     SingleBlog,
