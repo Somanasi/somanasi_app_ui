2dfec568565f1b400457a6ddbbd41f0ba1c4afd9
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-11 16:13:13 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React, {useState} from 'react'
+import React, {useReducer, useState} from 'react'
 import { Outlet } from 'react-router-dom'
 import { Footer, Navbar } from './components'
+import ApplicationStateReducer from './components/StateFunction';
 
 const App = () => {
   const [toggleMobileView, setToggleMobileView] = useState(false);
+  const [appState, dispatchAppState] = useReducer(
+    ApplicationStateReducer,
+    {
+      state_data: [],
+      toggleMobileView: false,
+    }
+  )
   return (
     <>
-      <Navbar  visible={toggleMobileView} toggle={setToggleMobileView}/>
+      <Navbar  visible={appState.toggleMobileView} toggle={dispatchAppState}/>
       <main>
         <Outlet/>
       </main>
--- src/components/StateFunction.js
+++ src/components/StateFunction.js
@@ -1,6 +1,11 @@
 const ApplicationStateReducer = (state, action) => {
     switch (action.type) {
         case 'TOGGLE_MOBILE_VIEW':
-            
+            return state = {
+                ...state,
+                toggleMobileView: !toggleMobileView
+            }
     }
 };
+
+export default ApplicationStateReducer;
