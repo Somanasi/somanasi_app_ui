3451720d510d50099e3b0d86e57a5a6bda576ce4
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-11 16:34:39 +0300

    converting the state management to to a complex state using reducer

--- src/components/StateFunction.js
+++ src/components/StateFunction.js
@@ -1,10 +1,17 @@
 const ApplicationStateReducer = (state, action) => {
     switch (action.type) {
+        // Toggling mobile devices
         case 'TOGGLE_MOBILE_VIEW':
             return state = {
                 ...state,
                 toggleMobileView: !state.toggleMobileView
-            }
+            };
+        // User login status
+        case 'USER_LOGIN_STATUS':
+            return state = {
+                ...state,
+                isLoggedIn: action.payload.isLoggedIn
+            };
     }
 };
 
