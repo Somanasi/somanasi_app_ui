03ea19fbf16c6a4067d358365646bd82e8de3b07
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 08:28:12 +0300

    structure state to a store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, } from "react-router-dom";
 import './assets/sass/main.scss'
-
+import routes from "./routes/routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
