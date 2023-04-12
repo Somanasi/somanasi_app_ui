64021e141f68706fb060a763656d0479ec8c67f7
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 08:21:07 +0300

    structure state to a store

---
+++ src/routes/routes.jsx
@@ -0,0 +1,25 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route
+  } from "react-router-dom";
+  import App from "../App";
+  import { Error, Home, Courses, Blog, AboutUs, ContactUs, SingleBlog, SingleCourse, Login, Logout, Register, Terms, } from "../views";
+  
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<App />} errorElement={<Error />} path="/">
+        <Route element={<Home />} index />
+        <Route element={<Courses />} path="courses" />
+        <Route element={<SingleCourse />} path="courses/:id" />
+        <Route element={<Blog />} path="blog" />
+        <Route element={<SingleBlog />} path="blog/:id"/>
+        <Route element={<AboutUs />} path="about_us" />
+        <Route element={<ContactUs />} path="contact_us" />
+        <Route element={<Login />} path="login" />
+        <Route element={<Register />} path="register" />
+        <Route element={<Logout />} path="logout" />
+        <Route element={<Terms />} path="terms" />
+      </Route>
+    )
+  );
