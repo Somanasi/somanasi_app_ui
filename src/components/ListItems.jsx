139e9e6b7e67dc56557d453fbfff8999b1695f71
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 10:47:33 +0300

    structure state to a store

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { uuid } from "uuidv4";
 import { useDispatchAppState } from '../store/ApplicationContext'
 
 const ListItems = ({children}) => {
   const toggle = useDispatchAppState();
   return (
-    <li onClick={ () => toggle({type: 'TOGGLE_MOBILE_VIEW'}) }>
+    <li onClick={ () => toggle({type: 'TOGGLE_MOBILE_VIEW'}) } key={uuid()}>
         {children}
     </li>
   )
