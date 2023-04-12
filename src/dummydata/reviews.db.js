d79e67b61ecc49b77b0541afbf22b5e2ee306089
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 08:51:27 +0300

    structure state to a store

--- src/dummydata/reviews.db.js
+++ src/dummydata/reviews.db.js
@@ -1,3 +1,34 @@
-export const  reviews = [
-    
+export const reviews = [
+    {
+        review_image: 'https://loremflickr.com/640/480/abstract',
+        name: 'random reviewer',
+        review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        laboriosam tempore earum odio expedita unde consectetur animi
+        sint? Magnam, veritatis. Temporibus, eaque sint enim qui quos esse
+        praesentium porro tempore.`
+    },
+    {
+        review_image: 'https://loremflickr.com/640/480/abstract',
+        name: 'random reviewer',
+        review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        laboriosam tempore earum odio expedita unde consectetur animi
+        sint? Magnam, veritatis. Temporibus, eaque sint enim qui quos esse
+        praesentium porro tempore.`
+    },
+    {
+        review_image: 'https://loremflickr.com/640/480/abstract',
+        name: 'random reviewer',
+        review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        laboriosam tempore earum odio expedita unde consectetur animi
+        sint? Magnam, veritatis. Temporibus, eaque sint enim qui quos esse
+        praesentium porro tempore.`
+    },
+    {
+        review_image: 'https://loremflickr.com/640/480/abstract',
+        name: 'random reviewer',
+        review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+        laboriosam tempore earum odio expedita unde consectetur animi
+        sint? Magnam, veritatis. Temporibus, eaque sint enim qui quos esse
+        praesentium porro tempore.`
+    }
 ]
