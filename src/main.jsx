12baa7501337162c357f0e285fafd1bd55f03f54
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 08:24:26 +0300

    structure state to a store

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
   RouterProvider,
-  Route
 } from "react-router-dom";
-import App from "./App";
-import { Error, Home, Courses, Blog, AboutUs, ContactUs, SingleBlog, SingleCourse, Login, Logout, Register, Terms, } from "./routes";
 import './assets/sass/main.scss'
 
-const routes = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<App />} errorElement={<Error />} path="/">
-      <Route element={<Home />} index />
-      <Route element={<Courses />} path="courses" />
-      <Route element={<SingleCourse />} path="courses/:id" />
-      <Route element={<Blog />} path="blog" />
-      <Route element={<SingleBlog />} path="blog/:id"/>
-      <Route element={<AboutUs />} path="about_us" />
-      <Route element={<ContactUs />} path="contact_us" />
-      <Route element={<Login />} path="login" />
-      <Route element={<Register />} path="register" />
-      <Route element={<Logout />} path="logout" />
-      <Route element={<Terms />} path="terms" />
-    </Route>
-  )
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
