57e03e8aa35d3aa0c0f38ad07d1f182058f12e65
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 10:14:02 +0300

    structure state to a store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React, {useReducer,} from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Footer, Navbar } from './components'
 import ApplicationStateReducer from './store/ApplicationContext';
 
 const App = () => {
-  const [appState, dispatchAppState] = useReducer(
-    ApplicationStateReducer,
-    {
-      
-    }
-  )
   return (
     <>
       <Navbar  visible={appState} toggle={dispatchAppState}/>
