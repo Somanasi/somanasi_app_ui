7a591d427f2d426afcda134c9f665ec7fd51b5e4
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 10:03:37 +0300

    structure state to a store

--- src/store/ApplicationContext.js
+++ src/store/ApplicationContext.js
@@ -1,2 +1,5 @@
 import { createContext } from "react";
 import { ApplicationStateReducer } from "./ApplicationStateReducer";
+
+const applicationState = createContext(null);
+const dispatchApplicationState = createContext(null);
