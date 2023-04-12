b6ca8205421691d045f9057733136cd1bb031369
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-12 10:53:50 +0300

    structure state to a store

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -5,7 +5,7 @@ import { useDispatchAppState } from '../store/ApplicationContext'
 const ListItems = ({children}) => {
   const toggle = useDispatchAppState();
   return (
-    <li onClick={ () => toggle({type: 'TOGGLE_MOBILE_VIEW'}) } key={uuidv4()}>
+    <li onClick={ () => toggle({type: 'TOGGLE_MOBILE_VIEW'}) }>
         {children}
     </li>
   )
