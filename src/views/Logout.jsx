21d2c00d1e69dff922e0e215c473c5c17038b0b7
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-13 16:05:32 +0300

    display reviews

--- src/views/Logout.jsx
+++ src/views/Logout.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useDispatchAppState } from '../store/ApplicationContext'
 
 const Logout = () => {
   console.log('user logged out')
+  const dispatchState = useDispatchAppState();
   return (
     <div>Logout</div>
   )
