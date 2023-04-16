8c9869dad1c283c2ad4f021c1ffadaacb33a81b2
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-04-16 17:27:02 +0300

    modified

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       </section>
       <section className="tips">
         <div className="section-heading">
-          <h1>Why Somesha Dada?</h1>
+          <h1>Why Elimuendelezi?</h1>
         </div>
         <div className="section-content">
           <div>
