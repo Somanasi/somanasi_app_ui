68e7250601e526c1c4625aa9a298abdff7c2a6f9
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-17 07:08:39 +0300

    add style to single course

--- src/views/SingleCourse.jsx
+++ src/views/SingleCourse.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from "react-router-dom";
 import { dummyData } from '../dummydata/Dummy.js';
-import { GoChevronLeft, GoKebabVertical } from "react-icons/go";
+import { BsChevronLeft, BsThreeDotsVertical } from "react-icons/bs";
 import { FaShoppingBag } from "react-icons/fa"
 import { Button, Course } from '../components';
 
@@ -18,21 +18,21 @@ const SingleCourse = () => {
     if(!course) return <div className='errorBoundary'><h1>Error</h1><p>Course Not Found!</p></div>;
 
     return (
-        <main className='single_course'>
-            <section className='course_header'>
-                <div>
-                    <Link to={'/courses'}>
-                        <GoChevronLeft size={30} />
+        <main className='single_course bg-primary'>
+            <section className='course_header px-2 py-4'>
+                <div className='course_header_column flex justify-start py-2 '>
+                    <Link to={'/courses'} className='text-warning'>
+                        <BsChevronLeft size={25} />
                     </Link>
                 </div>
-                <div>
-                    <h1>Course</h1>
+                <div className='course_header_column flex justify-center py-2 '>
+                    <h1 className='font-extrabold text-2xl text-warning'>Course</h1>
                 </div>
-                <div>
-                    <GoKebabVertical size={30} />
+                <div className='course_header_column text-right py-2 flex justify-end pr-1 text-warning'>
+                    <BsThreeDotsVertical size={25} />
                 </div>
             </section>
-            <section className='course_details'>
+            <section className='course_details py-4 px-2'>
                 {/*get single course info from database*/}
                 {dummyData.map((course, index) => (((course.id + '') === id) ?
                     <Course course={course} key={index} />
