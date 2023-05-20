9443e239802c517b10de251713cc09068c9580df
paullaster <paullasterokoth98@gmail.com>
2023-05-21 00:10:34 +0300

    set single course page

--- src/components/Courses/Course.jsx
+++ src/components/Courses/Course.jsx
@@ -1,68 +1,64 @@
-import React, { useState} from 'react'
+import React, { useState } from "react";
 import { CourseBreakDown } from "./index";
 import { Button } from "../index";
 const Course = ({ course }) => {
-    const [courseOpen, setCourseOpen] = useState(null);
-    const toggleCourseOpen = (e) => {
-        e.preventDefault();
-        setCourseOpen(!courseOpen);
-    };
+  const [courseOpen, setCourseOpen] = useState(null);
+  const toggleCourseOpen = (e) => {
+    e.preventDefault();
+    setCourseOpen(!courseOpen);
+  };
 
-    return (
-        <>
-            <div className='course_title rounded-lg py-4 '>
-                <div className='course_title_column pl-4 '>
-                    <div >
-                        {course.classification}
-                    </div>
-                    <div>
-                        <h2>{course.name}</h2>
-                    </div>
-                    <div>
-                        <span>KES. {course.price}</span>
-                    </div>
-                </div>
-                <div className='course_title_img_holder'>
-                    {/* <img src={course.course_avatart} alt={course.name} className='bg-information w-80' /> */}
-                </div>
-            </div>
-            <div className='course_description'>
-                <div>
-                    <div>
-                        <h3>Abour Course</h3>
-                    </div>
-                    <div>
-                        <span>
-                            &#9733; 4.4(700);
-                        </span>
-                    </div>
-                </div>
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Laborum odit itaque ut doloribus, nesciunt labore id
-                        doloremque saepe aliquid quisquam ipsam quia ea! Magnam
-                        harum ipsam iusto exercitationem quod suscipit!
-                    </p>
-                </div>
-            </div>
-            <div className=''>
-                <div>
-                    <h3>Course</h3>
-                </div>
-                <div>
-                    <Button onClick={(e) => toggleCourseOpen(e)}>See All</Button>
-                </div>
-            </div>
-            <div className="course_breakdown">
-                {
-                    course.course_breakdown.map((breakdown, index) => {
-                        return <CourseBreakDown breakdown={breakdown} key={index} />
-                    })
-                }
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="course_title rounded-lg py-4 ">
+        <div className="course_title_column pl-4 ">
+          <div>{course.classification}</div>
+          <div>
+            <h2>{course.name}</h2>
+          </div>
+          <div>
+            <span>KES. {course.price}</span>
+          </div>
+        </div>
+        <div className="course_title_img_holder">
+          {/* <img src={course.course_avatart} alt={course.name} className='bg-information w-80' /> */}
+        </div>
+      </div>
+      <div className="course_description py-8">
+        <div className="about_course pb-4">
+          <div>
+            <h3 className="font-bold text-warning">Abour Course</h3>
+          </div>
+          <div>
+            <span className="text-warning">&#9733;</span>
+            <span className="font-bold">4.4</span>
+            <span className="font-extralight">(700)</span>
+          </div>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            odit itaque ut doloribus, nesciunt labore id doloremque saepe
+            aliquid quisquam ipsam quia ea! Magnam harum ipsam iusto
+            exercitationem quod suscipit!
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <div>
+          <h3>Course</h3>
+        </div>
+        <div>
+          <Button onClick={(e) => toggleCourseOpen(e)}>See All</Button>
+        </div>
+      </div>
+      <div className="course_breakdown">
+        {course.course_breakdown.map((breakdown, index) => {
+          return <CourseBreakDown breakdown={breakdown} key={index} />;
+        })}
+      </div>
+    </>
+  );
+};
 
-export default Course
+export default Course;
