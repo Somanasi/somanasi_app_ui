b52a7052284e07118c4d90be2724b8c3dd7ed37c
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-26 06:34:20 +0300

    ui single course

--- src/components/Courses/CourseBreakDown.jsx
+++ src/components/Courses/CourseBreakDown.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { GoChevronRight } from 'react-icons/go';
+import { GoChevronRight, GoChevronUp } from 'react-icons/go';
 import  WeeklyLessons  from './WeeklyLessons';
 
 const CourseBreakDown = ({ breakdown }) => {
@@ -11,13 +11,18 @@ const CourseBreakDown = ({ breakdown }) => {
   };
 
   return (
-    <div className='course_breakdown_card py-2 rounded-lg'>
+    <div className='course_breakdown_card px-2 py-2 rounded-lg'>
       <div onClick={handleLessonView} className='course_breakdown_card_navigation'>
         <div className='course_breakdown_card_navigation_column tag pl-1'>
           <h2 className='font-bold text-lg'>{breakdown.tag}</h2>
         </div>
         <div className='course_breakdown_card_navigation_column icon pr-2'>
+         {
+          seeLessons ? 
+          <GoChevronUp size={30} className='text-warning' />
+          :
           <GoChevronRight size={30} className='text-warning' />
+         }
         </div>
       </div>
       {
