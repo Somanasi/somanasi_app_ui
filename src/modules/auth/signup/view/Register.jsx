02cab8f1b01b42d277ab2e850889c2925c1e403d
Paullaster Okoth <43445963+paullaster@users.noreply.github.com>
2023-06-03 00:52:25 +0300

    Development (#20)
    * fix
    
    * fix
    
    * fix
    
    * add style to single course
    
    * add style to single course
    
    * set single course page
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up

--- src/modules/auth/signup/view/Register.jsx
+++ src/modules/auth/signup/view/Register.jsx
@@ -5,39 +5,64 @@ import Button from '../../../shared/components/Button'
 
 const Register = () => {
   return (
-    <main className='login'>
-      <section className='user_login'>
-        <div className='section-heading'>
-          <h1>Register for Somesha Dada</h1>
+    <main className='login py-8 px-2 bg-primary'>
+      <section className='user_login py-4 px-8'>
+        <div className='section-heading text-center pb-1'>
+          <h1 className='font-light text-lg text-tertiary'>Registration</h1>
         </div>
+        <hr />
         <div className="user_login__form">
           <Form method='POST'>
-            <div className="form-group">
-              <label htmlFor="user_type">
-                Register as a
-              </label>
-              <select name="user_type" id="user_type">
-                <option value="" selected disabled>Select a user type</option>
-                <option value="student">Student</option>
-                <option value="parent" disabled>Parent</option>
-                <option value="school" disabled>School</option>
-              </select>
+            <div className="form-group user_type_groups py-4 ">
+              {/* <div>
+                <input type="radio" name="user_type" id="admin" value={'admin'} />
+                <label htmlFor="admin">Admin</label>
+              </div> */}
+              <div>
+                <input type="radio" name="user_type" id="workspace" value={'wrokspace'} required/>
+                <label htmlFor="workspace">Workspace</label>
+              </div>
+              <div>
+                <input type="radio" name="user_type" id="student" value={'student'} required/>
+                <label htmlFor="student">Student</label>
+              </div>
             </div>
-            <InputWithLabel name={'userId'} id={'username'} placeholder={'Enter your full name'}>
-              Name
+            <div className="name">
+              <InputWithLabel name={'user_firstname'} id={'username'} placeholder={'Enter first name'} required={ true}>
+                First Name
+              </InputWithLabel>
+              <InputWithLabel name={'user_lastname'} id={'username'} placeholder={'Enter last name'} required={ true}>
+                Last  Name
+              </InputWithLabel>
+              <InputWithLabel name={'user_othername'} id={'username'} placeholder={'other name'}>
+                Other Name
+              </InputWithLabel>
+            </div>
+            <InputWithLabel type={'email'} name={'user_email'} id={'phoneNumber'} placeholder={'otherperson@someone.com'} required={ true}>
+              Email
             </InputWithLabel>
-            <InputWithLabel name={'userId'} id={'phoneNumber'} placeholder={'0700268088'}>
-             Phone number
+            <InputWithLabel type={'tel'} name={'userId'} id={'phoneNumber'} placeholder={'0700268088'} required={ true}>
+              Phone number
             </InputWithLabel>
-            <InputWithLabel type={'password'} name={'password'} id={'password'}>
+            <InputWithLabel type={'password'} name={'password'} id={'password'} required={ true}>
               password
             </InputWithLabel>
-            <InputWithLabel type={'password'} name={'confirmPassword'} id={'reTypePassword'}>
+            <InputWithLabel type={'password'} name={'confirmPassword'} id={'reTypePassword'} required={ true}>
               Re-type password
             </InputWithLabel>
-            <div className="form-group">
+            <div className="gender py-6">
+              <div>
+                <input type="radio" name="gender" id="female" required/>
+                <label htmlFor="female">Female</label>
+              </div>
+              <div>
+                <input type="radio" name="gender" id="male" required/>
+                <label htmlFor="male">Male</label>
+              </div>
+            </div>
+            <div className="form-group terms_and_policy mb-6">
               <input type="checkbox" />
-              <label htmlFor=""> I agree to <NavLink to={'terms'}>terms and conditions</NavLink></label>
+              <label htmlFor=""><span className='mr-1'> I agree to </span> <NavLink to={'terms'}> terms and conditions</NavLink></label>
             </div>
             <Button type='submit'>Sign up</Button>
           </Form>
--- src/modules/shared/components/InputWithLabel.jsx
+++ src/modules/shared/components/InputWithLabel.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const InputWithLabel = ({name, type='text', id, placeholder, children}) => {
+const InputWithLabel = ({name, type='text', id, placeholder, required=false, children}) => {
   return (
     <div className='form-group'>
         <label htmlFor={id}>{children}</label>
-        <input type={type} name={name} id={id} placeholder={placeholder}/>
+        <input type={type} name={name} id={id} placeholder={placeholder} required={required}/>
     </div>
   )
 }
