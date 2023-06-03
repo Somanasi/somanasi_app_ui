a1e0b54d4af900256c99e2776251bf35e32436c3
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-06-03 07:06:55 +0300

    sign up

--- src/modules/auth/login/view/Login.jsx
+++ src/modules/auth/login/view/Login.jsx
@@ -5,11 +5,12 @@ import Button from '../../../shared/components/Button'
 
 const Login = () => {
   return (
-    <main className='login'>
-      <section className='user_login'>
-        <div className='section-heading'>
+    <main className='login py-8 px-2 bg-primary'>
+      <section className='user_login py-4 px-8'>
+        <div className='section-heading text-center pb-1'>
           <h1>Login to Somesha Dada</h1>
         </div>
+        <hr />
         <div className="user_login__form">
           <Form method='POST'>
             <InputWithLabel name={'userId'} id={'username'}>
@@ -18,13 +19,15 @@ const Login = () => {
             <InputWithLabel type={'password'} name={'password'} id={'password'}>
               password
             </InputWithLabel>
-            <Button type='submit'>Continue</Button>
+            <div className="form-group">
+              <Button type='submit'>login</Button>
+            </div>
           </Form>
         </div>
         <div className='new_user'>
           <p>
-           <span> New to our platform?</span> 
-          <NavLink to={'/register'}>Register Here</NavLink>
+            <span> New to our platform?</span>
+            <NavLink to={'/register'}>Register Here</NavLink>
           </p>
         </div>
       </section>
