d810d7e4e93d4bd5bb1203156a6bd90e21674fc3
paullaster <paullasterokoth98@gmail.com>
2023-06-05 07:39:02 +0300

    responder  service

--- src/modules/shared/services/responderService.jsx
+++ src/modules/shared/services/responderService.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+/**
+ * Require two parameters:
+ * @param {*} event -> Can either be onSuccess or onError.
+ * @param {*} message -> Any message to display.
+ */
 export const Event = (event, message) => {
   const onSuccess = () =>
     toast.success(message, {
