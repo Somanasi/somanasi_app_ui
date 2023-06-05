40552b60103505ffa4e3fd217043d66232b0f383
paullaster <paullasterokoth98@gmail.com>
2023-06-05 08:43:28 +0300

    registration  service

--- src/modules/auth/signup/services/registerAction.jsx
+++ src/modules/auth/signup/services/registerAction.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useActionData } from 'react-router-dom';
-
+// import {}
 const registerAction = async({request}) => {
     const payload = Object.fromEntries(await request.formData());
     console.log();
