2684fd225cd349c896790dd4d87392417e7ee9e9
paullaster <paullasterokoth98@gmail.com>
2023-06-05 07:34:52 +0300

    responder  service

--- src/modules/shared/services/responderService.jsx
+++ src/modules/shared/services/responderService.jsx
@@ -1,3 +1,65 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export const Event = (event, message) => {
+  const onSuccess = () =>
+    toast.success(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored"
+    });
 
+  const onError = () =>
+    toast.error(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored"
+    });
+  retrun(
+    <div>
+      {event === "onSuccess" ? (
+        <>
+          {onSuccess()}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </>
+      ) : (
+        <>
+          {onError()}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </>
+      )}
+    </div>
+  );
 };
