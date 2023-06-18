a8effcd146d1e90d6882006c2bec0519e1bf7e4e
Software Developer <douglas1999michael@gmail.com>
2023-06-18 23:44:51 +0300

    dashboard changes

--- src/modules/dashboard/components/charts/graphs-init.jsx
+++ src/modules/dashboard/components/charts/graphs-init.jsx
@@ -1,42 +1,42 @@
-// import React from "react";
-// import { Bar } from "react-chartjs-2";
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-// const BarChart = () => {
-//   const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-//   const data = {
-//     labels: labels,
-//     datasets: [
-//       {
-//         label: "Study time analytics",
-//         backgroundColor: "rgb(255, 99, 132)",
-//         borderColor: "rgb(255, 99, 132)",
-//         data: [15, 10, 5, 2, 20, 30, 45, 20, 23, 21, 30, 28],
-//       },
-//     ],
-//   };
-//   return (
-//     <div>
-//       <Bar data={data} />
-//     </div>
-//   );
-// };
+const BarChart = () => {
+  const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Study time analytics",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [15, 10, 5, 2, 20, 30, 45, 20, 23, 21, 30, 28],
+      },
+    ],
+  };
+  return (
+    <div>
+      <Bar data={data} />
+    </div>
+  );
+};
 
-// export default BarChart;
+export default BarChart;
--- src/modules/dashboard/components/layouts/MainApp.jsx
+++ src/modules/dashboard/components/layouts/MainApp.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { FaBook, FaFilter, FaLifeRing, FaSearch } from 'react-icons/fa'
+import { FaClock, FaEllipsisV, FaEnvelope, FaFilter, FaHome, FaLifeRing, FaReact, FaSearch, FaVectorSquare } from 'react-icons/fa'
 import { DashboardCardData } from '../../data/cards'
-// import { FaBell, FaChevronCircleRight, FaFilter, FaSearch } from "react-icons/fa";
-// import BarChart from '../charts/graphs-init';
-// import { DashboardCardData } from '../../data/cards';
+import BarChart from '../charts/graphs-init';
 
 
 const MainApp = () => {
@@ -35,8 +33,8 @@ const MainApp = () => {
 
 
         {/* Display section */}
-        <main className='w-full'>
-          <setion className="bg-blue-400 grid lg:grid-cols-4 gap-1 py-2 px-6 h-12 mt-2 rounded-tl-md rounded-tr-md relative">
+        <main className='w-full lg:px-10'>
+          <setion className="bg-slate-50 grid lg:grid-cols-4 gap-1 py-2 px-6 h-24 mt-2 rounded-tl-md rounded-tr-md relative mb-4">
             {
               DashboardCardData.map((data, id) => {
                 return   <article className='p-2 shadow-md rounded-md mt-2 bg-white flex flex-row justify-around items-start border-l-8 border-orange-500 mx-auto' key={id}>
@@ -45,13 +43,114 @@ const MainApp = () => {
                   <small className='font-bold py-1'>{data.cardCapacity}</small>
                   <small className='font-bold py-1'>{data.time}</small>
                 </div>
-                <div className='w-12 h-12 flex justify-center items-center ml-4 text-4xl'>{data.cardIcon}</div>
+                <div className='w-12 h-12 flex justify-center items-center ml-4 text-4xl cursor-pointer'>{data.cardIcon}</div>
               </article>
               })
             }
           </setion>
 
-          <setion></setion>
+
+
+          {/* Graph visualization */}
+          <section className='py-4 px-2 mt-6 grid lg:grid-cols-2 sm:grid-col-1 gap-4'>
+            <div className='shadow-md bg-slate-50 py-4 px-2'>
+              <div className='flex flex-row justify-between items-center border-b-2 border-gray-100 py-1'>
+                <h1 className='font-bold'>Visualize ~ Studies</h1>
+                <FaEllipsisV/>
+              </div>
+              <div className='w-full overflow-hidden h-60 px-2'>
+               <BarChart />
+              </div>
+            </div>
+
+
+            {/* Table section */}
+            <div className='shadow-md bg-slate-50 py-4 px-2'>
+              <div className='flex flex-row justify-between items-center border-b-2 border-gray-100 py-1'>
+                <h1 className='font-bold'>Table ~ Courses</h1>
+                <FaEllipsisV/>
+              </div>
+              <div className='w-full overflow-hidden h-60 px-2'>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg h-100">
+                  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 h-full">
+
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-5 dark:text-gray-400">
+                      <tr>
+                        <th scope="col" class="px-6 py-3">Course Name</th>
+                        <th scope="col" class="px-6 py-3">
+                            <div class="flex items-center">Units</div>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <div class="flex items-center">Length</div>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            <div class="flex items-center">Price</div>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                          <div class="flex items-center">Action</div>
+                        </th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      <tr class="border-b dark:border-gray-700 bg-slate-400">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Computer Science</th>
+                        <td class="px-6 py-4 dark:text-white">48</td>
+                        <td class="px-6 py-4 dark:text-white">3hrs/day</td>
+                        <td class="px-6 py-4 dark:text-white">$2999</td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                      </tr>
+                      <tr class="border-b dark:border-gray-700 bg-slate-400">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Nursing</th>
+                        <td class="px-6 py-4 dark:text-white">32</td>
+                        <td class="px-6 py-4 dark:text-white">3hrs/day</td>
+                        <td class="px-6 py-4 dark:text-white">$20300</td>
+                        <td class="px-6 py-4 text-right">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                      </tr>
+                      
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+
+          {/* Links Available */}
+          <section className='p2'>
+           <div className='shadow-md bg-slate-50 py-4 px-2'>
+              <div className='flex flex-row justify-between items-center border-b-2 border-gray-100 py-1'>
+                <h1 className='font-bold'>Links ~ History</h1>
+                <FaEllipsisV/>
+              </div>
+              <div className='w-full overflow-hidden px-2'>
+                <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 mt-1">
+                  <li class="mr-2">
+                    <a href="#" class="px-4 py-3 hover:text-white bg-gray-200 text-black hover:bg-blue-600 rounded-full active flex flex-row items-center" aria-current="page"> <FaHome/>&nbsp;Home Packages</a>
+                  </li>
+                  <li class="mr-2">
+                    <a href="#" class="px-4 py-3 hover:text-white bg-gray-200 text-black hover:bg-blue-600 rounded-full active flex flex-row items-center" aria-current="page"> <FaVectorSquare/>&nbsp;Constructions</a>
+                  </li>
+                  <li class="mr-2">
+                    <a href="#" class="px-4 py-3 hover:text-white bg-gray-200 text-black hover:bg-blue-600 rounded-full active flex flex-row items-center" aria-current="page"> <FaReact/>&nbsp;Reactions</a>
+                  </li>
+                  <li class="mr-2">
+                    <a href="#" class="px-4 py-3 hover:text-white bg-gray-200 text-black hover:bg-blue-600 rounded-full active flex flex-row items-center" aria-current="page"> <FaEnvelope/>&nbsp;Comments</a>
+                  </li>
+                  <li class="mr-2">
+                    <a href="#" class="px-4 py-3 hover:text-white bg-gray-200 text-black hover:bg-blue-600 rounded-full active flex flex-row items-center" aria-current="page"> <FaClock/>&nbsp;Time elapse</a>
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+          </section>
+
         </main>
       </article>
     </>
