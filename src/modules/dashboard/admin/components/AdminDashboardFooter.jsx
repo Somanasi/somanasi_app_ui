7631e9fdf5b7eb749ecc18fdd55dbedb56254f02
paullaster <paullasterokoth98@gmail.com>
2023-06-30 06:59:48 +0300

    setting admin scafolding admin dashboard

---
+++ src/modules/dashboard/admin/components/AdminDashboardFooter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDashboardFooter = () => {
+  return (
+    <footer>AdminDashboardFooter</footer>
+  )
+}
+
+export default AdminDashboardFooter
--- src/modules/dashboard/admin/views/AdminDashoboard.jsx
+++ src/modules/dashboard/admin/views/AdminDashoboard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { AdminDashboardStateProvider } from '../adminStore'
+import {Outlet} from 'react-router-dom'
+import SideBar from '../components/SideBar'
+import MainAdminDashboardApp from '../components/MainAdminDashboardApp'
+import AdminDashboardFooter from '../components/AdminDashboardFooter'
 
 const AdminDashoboard = () => {
   return (
-    <div>AdminDashoboard</div>
+    <AdminDashboardStateProvider>
+        <SideBar />
+        <MainAdminDashboardApp />
+        <Outlet />
+        <AdminDashboardFooter />
+    </AdminDashboardStateProvider>
   )
 }
 
