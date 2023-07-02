2314d5c7829195f17becc72ad3255b3cb1263f40
BeMa99 <berclaym@gmail.com>
2023-07-02 20:54:43 +0300

    merge commit

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,8 +4,6 @@ import {
     Route
   } from "react-router-dom";
   import App from "../App";
-    
-  import { Error, Home, Courses, Blog, AboutUs, ContactUs, SingleBlog, SingleCourse, Login, Lesson, Quizes, Register, Terms, Dashboard, AdminDashoboard, } from "../views";
 
   import { 
     Error, 
@@ -49,36 +47,22 @@ import {
         <Route element={<AboutUs />} path="about_us" />
         <Route element={<ContactUs />} path="contact_us" />
         <Route element={<Login />} path="login" action={loginAction}/>
-      <>
-        <Route element={<App />} errorElement={<Error />} path="/">
-          <Route element={<Home />} index />
-          <Route element={<Courses />} path="courses"/>
-          <Route element={<SingleCourse />} path="courses/:id" />
-          <Route element={<Blog />} path="blog" />
-          <Route element={<SingleBlog />} path="blog/:id"/>
-          <Route element={<AboutUs />} path="about_us" />
-          <Route element={<ContactUs />} path="contact_us" />
-          <Route 
-            element={<Login />} 
-            path="login" 
-            action={loginAction} 
-          />
 
-          <Route element={<Register />} 
-          path="register"
-          action={registerAction}
-          />
-          <Route element={<Terms />} path="terms" />
+        <Route element={<Register />} 
+        path="register"
+        action={registerAction}
+        />
+        <Route element={<Terms />} path="terms" />
 
-          {/* Portal */}
-          <Route element={<Dashboard />} path="dashboard">
-            <Route element={<DashboardHome />} index />
-            <Route element={<UserProfile />} path="profile" />
-            <Route element={<ChangePassword />} path="changepassword" />
-            <Route element={<Courseonoffer />} path="coursesonoffer" />
-            <Route element={<Blogpage />} path="blogpage" />
-            <Route element={<Generatereport />} path="downloadcoursesummary" />
-          </Route>
+        {/* Portal */}
+        <Route element={<Dashboard />} path="dashboard">
+          <Route element={<DashboardHome />} index />
+          <Route element={<UserProfile />} path="profile" />
+          <Route element={<ChangePassword />} path="changepassword" />
+          <Route element={<Courseonoffer />} path="coursesonoffer" />
+          <Route element={<Blogpage />} path="blogpage" />
+          <Route element={<Generatereport />} path="downloadcoursesummary" />
+        </Route>
 
         <Route 
           element={<AdminDashoboard />} 
@@ -90,7 +74,6 @@ import {
           <Route element={<ContentStat />} path="content/stats" />
         </Route>
       </Route>
-      </>
     )
   );
 
