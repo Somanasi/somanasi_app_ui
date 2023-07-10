8a3b8ff3b84c2afdb6b072864434fe03babc138a
Paullaster Okoth <43445963+paullaster@users.noreply.github.com>
2023-07-10 06:06:18 +0300

    Development (#47)
    * fix
    
    * fix
    
    * fix
    
    * add style to single course
    
    * add style to single course
    
    * set single course page
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * single course ui
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * ui single course
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * buy btns
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * user accounts
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * adding sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * sign up
    
    * axios service
    
    * axios service
    
    * axios service
    
    * axios service
    
    * responder  service
    
    * responder  service
    
    * responder  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * registration  service
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * add changes
    
    * re-organizing the architecture
    
    * admin user
    
    * update the login logic
    
    * add token and user
    
    * add token and user
    
    * add token and user
    
    * setting admin scafolding admin dashboard
    
    * temp dashboard
    
    * code quality set-up and admin dashboard update
    
    * code quality set-up and admin dashboard update
    
    * admin dashboard update
    
    * admin dashoboard update
    
    * create course : update

--- src/modules/dashboard/admin/modules/content/views/CreateContent.jsx
+++ src/modules/dashboard/admin/modules/content/views/CreateContent.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Form } from 'react-router-dom'
+import {InputWithLabel} from '../../../../../shared'
+import Lesson from '../components/Lesson'
 
 const CreateContent = () => {
   return (
-    <div>CreateContent</div>
+    <main>
+      <section className="">
+        <h1>Create a new course</h1>
+      </section>
+      <section className="">
+          <Form>
+            <fieldset>
+              <legend>Course information</legend>
+              <InputWithLabel name="course_title" placeholder="Course Title" required={true} />
+              <InputWithLabel name="course_descrition" placeholder="Course description" required={true} />
+              <select name="course_category" placeholder="Course category">
+                  <option value=""> Please select a course category</option>
+              </select>
+            </fieldset>
+            <fieldset>
+              <legend>Lesson</legend>
+              <input list={"course_level"} name={"course_level"} id={"course_level"} placeholder={"Course Level"} />
+              <datalist id={"course_level"}>
+                <option value="Week 1">Week 1</option>
+              </datalist>
+              <Lesson/>
+            </fieldset>
+          </Form>
+      </section>
+    </main>
   )
 }
 
