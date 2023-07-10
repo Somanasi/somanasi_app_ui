b48ea381617904b79477f85f954fb0749d50039d
paullaster <paullasterokoth98@gmail.com>
2023-07-10 06:51:59 +0300

    create course : update

--- src/modules/dashboard/admin/modules/content/components/Lesson.jsx
+++ src/modules/dashboard/admin/modules/content/components/Lesson.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { InputWithLabel } from '../../../../../shared'
+import LessonTextEditor from './LessonTextEditor'
 
 const Lesson = () => {
   return (
     <>
     <InputWithLabel name={"lesson_title"} placeholder={"Lesson Title"}/>
     <InputWithLabel name={"lesson_description"} placeholder={"Lesson Description"}/>
+    <LessonTextEditor />
     </>
   )
 }
